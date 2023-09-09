4b4cb323fc431f04d420c9f489f4ecbd9dd1e108
tokiya0219 <53889976+tokiya0219@users.noreply.github.com>
2023-09-09 18:09:38 +0900

    apollo lesson 1

---
+++ client/codegen.ts
@@ -0,0 +1,17 @@
+import { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: "http://localhost:4000",
+  // the files, want to scan
+  documents: ["src/**/*.tsx"],
+  generates: {
+    "./src/__generated__/": {
+      preset: "client",
+      presetConfig: {
+        gqlTagName: "gql",
+      },
+    },
+  },
+};
+
+export default config;
